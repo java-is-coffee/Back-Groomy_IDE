8a9fb9564a5649337a0ada35c4497a09ea7fe911
Park SangHyeon <tkrhkrkfn@naver.com>
2024-01-31 18:18:20 +0900

    1차 기본세팅

---
+++ src/main/java/Javaiscoffee/Groomy/IDE/GroomyIdeApplication.java
@@ -0,0 +1,13 @@
+package Javaiscoffee.Groomy.IDE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroomyIdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroomyIdeApplication.class, args);
+	}
+
+}
