8641a87521291f7e3d30cf55ec8380226f686825
Jiho Yoon <j5i3h8o8@gmail.com>
2024-02-05 13:08:49 +0900

    1차 기본세팅

---
+++ src/test/java/javaiscoffee/groomy/ide/GroomyIdeApplicationTest.java
@@ -0,0 +1,14 @@
+package javaiscoffee.groomy.ide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GroomyIdeApplicationTest {
+
+    @Test
+    public void test() {
+
+    }
+
+}
