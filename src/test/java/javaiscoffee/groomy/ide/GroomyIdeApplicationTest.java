6c825a69672b555ee56b3c6c0ed36a2eaa93f5a4
Park SangHyeon <tkrhkrkfn@naver.com>
2024-02-05 15:27:38 +0900

    FIX application.properties랑 테스트클래스 설정
    테스트를 위한 임의 키값을 설정

--- src/test/java/javaiscoffee/groomy/ide/GroomyIdeApplicationTest.java
+++ src/test/java/javaiscoffee/groomy/ide/GroomyIdeApplicationTest.java
@@ -2,10 +2,13 @@ package javaiscoffee.groomy.ide;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@TestPropertySource(properties = {"jwt.secret=3123758a0d7ef02a46cba8bdd3f898dec8afc9f8470341af789d59f3695093be"})
 public class GroomyIdeApplicationTest {
 
     @Test
