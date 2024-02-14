4b2694b9a02f9afd60b233cae0d844b5225afa9e
Park SangHyeon <tkrhkrkfn@naver.com>
2024-02-14 11:47:19 +0900

    FIX / 스프링 시큐리티 설정 변경

--- src/main/java/javaiscoffee/groomy/ide/websocket/WebSocketConfig.java
+++ src/main/java/javaiscoffee/groomy/ide/websocket/WebSocketConfig.java
@@ -20,7 +20,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws/{projectId}").setAllowedOriginPatterns("*").withSockJS();
-        registry.addEndpoint("/ws/{projectId}").setAllowedOriginPatterns("*");
     }
 
     @Override
