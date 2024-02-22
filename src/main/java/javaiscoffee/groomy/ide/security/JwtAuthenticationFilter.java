1b6bf0d23dcfed2810ae95e68b062b5e955797f0
Park SangHyeon <tkrhkrkfn@naver.com>
2024-02-22 15:38:57 +0900

    FIX / GI-37 OAUTH URI 보안 수정

--- src/main/java/javaiscoffee/groomy/ide/security/JwtAuthenticationFilter.java
+++ src/main/java/javaiscoffee/groomy/ide/security/JwtAuthenticationFilter.java
@@ -39,7 +39,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         if (requestURI.startsWith("/api/member/login") || "/api/member/register".equals(requestURI)
                 || "/api/member/refresh".equals(requestURI) || "/api/member/register/email-check".equals(requestURI)
                 || requestURI.startsWith("/ws") || requestURI.startsWith("/YJS")
-                || "/auth/google".equals(requestURI) || requestURI.startsWith("/api/email")) {
+                || requestURI.startsWith("/auth") || requestURI.startsWith("/api/email")) {
             filterChain.doFilter(request, response);
             return;
         }
