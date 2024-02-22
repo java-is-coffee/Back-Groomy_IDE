17253457a100698e2084f86ee813fd7b1f2e546f
roaroa <hyh1216@naver.com>
2024-02-22 16:26:59 +0900

    Feat : GI-37 구글로그인 리디렉션 URI 수정

--- src/main/java/javaiscoffee/groomy/ide/login/oauth/controller/OAuthController.java
+++ src/main/java/javaiscoffee/groomy/ide/login/oauth/controller/OAuthController.java
@@ -32,7 +32,7 @@ public class OAuthController {
      * @param code
      * @return
      */
-    @GetMapping("/login/oauth2/code/{socialType}")
+    @GetMapping("/login/oauth2/{socialType}")
     public String callback(@PathVariable(name = "socialType") SocialType socialType, @RequestParam(name = "code") String code) {
         // OAuth 콜백에서 전달된 인증 코드를 사용하여 엑세스 토큰 요청
         // 엑세스 토큰 요청 및 사용 후 응답 처리
